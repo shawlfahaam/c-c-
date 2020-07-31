ac985f100752960f5e1297048def5ceaf7cf6229
shawlfahaam <68328621+shawlfahaam@users.noreply.github.com>
2020-07-31 18:44:53 +0530

    Add files via upload
    sum of 3 numbers using a single statement.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c,d;
+printf("enter the values of a,b & c\n");
+scanf("%d %d %d",&a,&b,&c);
+d=(a+b+c);
+printf("SUM of %d,%d and %d is %d",a,b,c,d);
+
+    return 0;
+}
