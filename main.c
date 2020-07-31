799c4fa1c013029349781aee8b8d0254f241563e
Fahaam Shawl <shawlfahaam@gmail.com>
2020-07-31 22:44:06 +0530

    Add files via upload
    simple calculator by using switch statement

--- main.c
+++ main.c
@@ -3,11 +3,33 @@
 
 int main()
 {
-int a,b,c,d;
-printf("enter the values of a,b & c\n");
-scanf("%d %d %d",&a,&b,&c);
-d=(a+b+c);
-printf("SUM of %d,%d and %d is %d",a,b,c,d);
 
-    return 0;
+   char Calculate;
+   float a, b, result = 0;
+   printf("Enter the operation to be performed (+, -, *, /)\n");
+   scanf("%c", &Calculate);
+   printf("\n Enter the numbers: a and b");
+   scanf("%f%f", &a, &b);
+
+   switch(Calculate)
+  {
+  case '+':
+  result = a + b;
+  break;
+  case '-':
+  result = a - b;
+  break;
+  case '*':
+  result = a * b;
+  break;
+  case '/':
+  result = a/ b;
+  break;
+	default:
+	printf("\n You have enetered an Invalid Operator ");
+	}
+
+	printf("\n The result of %.2f %c %.2f  = %.2f", a,Calculate,b,result);
+
+return 0;
 }
