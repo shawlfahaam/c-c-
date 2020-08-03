c71c50d5dc6613222204e032e0868e52c3c6ccd7
Fahaam Shawl <shawlfahaam@gmail.com>
2020-08-03 10:17:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{ //have used typeof to rename the structure so that while in main it gets easy to type.
+
+    char name[30];
+    int id;
+    float salary;
+    char experience[50];
+
+} Employee;
+
 int main()
 {
+    int i, n=5;
+    Employee details[n];
+    printf("Enter %d Employee Details \n \n",n);
+    for(i=0; i<n; i++){
 
-   char Calculate;
-   float a, b, result = 0;
-   printf("Enter the operation to be performed (+, -, *, /)\n");
-   scanf("%c", &Calculate);
-   printf("\n Enter the numbers: a and b");
-   scanf("%f%f", &a, &b);
+        printf("Employee %d:- \n",i+1);
+        printf("Name: ");
+        scanf("%s",details[i].name);
+        printf("Id: ");
+        scanf("%d",&details[i].id);
+        printf("Salary: ");
+        scanf("%f",&details[i].salary);
+        printf("Experience: ");
+        scanf("%s",details[i].experience);
+        printf("\n");
+    }
 
-   switch(Calculate)
-  {
-  case '+':
-  result = a + b;
-  break;
-  case '-':
-  result = a - b;
-  break;
-  case '*':
-  result = a * b;
-  break;
-  case '/':
-  result = a/ b;
-  break;
-	default:
-	printf("\n You have enetered an Invalid Operator ");
-	}
+    printf("-------------- All Employees Details ---------------\n");
 
-	printf("\n The result of %.2f %c %.2f  = %.2f", a,Calculate,b,result);
+    for(i=0; i<n; i++){
 
-return 0;
+        printf("Name \t: ");
+        printf("%s \n",details[i].name);
+
+        printf("Id \t: ");
+        printf("%d \n",details[i].id);
+
+        printf("Salary \t: ");
+        printf("%f \n",details[i].salary);
+
+        printf("Experience\t: ");
+        printf("%s \n",details[i].experience);
+
+        printf("\n");
+    }
+
+    return 0;
+
 }
